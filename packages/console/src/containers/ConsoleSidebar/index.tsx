b143c1119ec088e069d12b520aba4d803d803242
pymba86 <pymba-96@mail.ru>
2024-06-09 14:58:27 +0500

    feat: react

---
+++ packages/console/src/containers/ConsoleSidebar/index.tsx
@@ -0,0 +1,10 @@
+import {useTranslation} from "react-i18next";
+
+
+export function ConsoleSidebar() {
+    const {t} = useTranslation(undefined, {
+        keyPrefix: 'console.tab_sections'
+    })
+
+
+}
